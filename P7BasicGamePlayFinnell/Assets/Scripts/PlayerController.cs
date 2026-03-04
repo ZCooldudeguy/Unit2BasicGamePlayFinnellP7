b7582dc0acd5eae92786e154ccda74efdf4c8e82
ZCooldudeguy <731256@student.dusd.net>
2026-03-04 15:23:27 -0800

    e
    started the first script and add characters

---
+++ P7BasicGamePlayFinnell/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float speed = 10.0f;
+    public float xRange = 10;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if transform.position.x < xRange)
+        {
+
+        }
+
+    }
+}
