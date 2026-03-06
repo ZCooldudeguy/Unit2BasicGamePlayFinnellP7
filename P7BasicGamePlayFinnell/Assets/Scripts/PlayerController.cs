45f119dcb6075b036b3ad0c054836ec2a641e256
ZCooldudeguy <731256@student.dusd.net>
2026-03-06 15:05:48 -0800

    e
    almost finished did script for launching food, moving animals and spawning animals.

--- P7BasicGamePlayFinnell/Assets/Scripts/PlayerController.cs
+++ P7BasicGamePlayFinnell/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ public class PlayerController : MonoBehaviour
     public float horizontalInput;
     public float speed = 10.0f;
     public float xRange = 10;
+    public GameObject projectilePrefab;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -15,9 +16,17 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if transform.position.x < xRange)
+        if (transform.position.x < -xRange)
         {
-
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
         }
 
     }
